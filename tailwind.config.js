4f1b7a4d8ad3364ad2d7f2bd73264c335852f858
yigitkg <yigitkg@gmail.com>
2023-06-08 00:23:29 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        indigonight: '#1f00cc',
+        linkedin: '#0077B5',
+      },
+      spacing: {
+        '15': '3.75rem',
+      },
+      zIndex: {
+        '-1': '-1',
+      },
+    },
+  },
+  plugins: [],
+}
+
