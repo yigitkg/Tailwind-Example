5f3ad06f1a6ff070a16e356a8aa5f54070c2287a
yigitkg <yigitkg@gmail.com>
2023-06-09 21:26:57 +0300

    plugin added, responsive design completed, modal added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const _ = require('lodash');
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   mode: 'jit',
   darkMode: 'class',
@@ -17,6 +20,23 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities, theme, e }) {
+      const calcUtilities = _.map(theme('spacing'), (value, key) => {
+        return {
+          [`.${e(`calc-h-full-${key}`)}`]: {
+            height: `calc(100% - ${value})`
+          },
+          [`.${e(`calc-w-full-${key}`)}`]: {
+            width: `calc(100% - ${value})`
+          }
+        }
+      })
+
+      addUtilities(calcUtilities, {
+        variants: ['responsive', 'hover'],
+      })
+    })
+  ],
 }
 
